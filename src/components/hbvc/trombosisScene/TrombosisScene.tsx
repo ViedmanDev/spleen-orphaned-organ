8acb23379c6786b0f1ba16fbfff7cfe0ecc37575
hiderbrandon <hiderbrandon@gmail.com>
2025-07-18 06:34:10 -0500

    added scene atmosfere to 1st escene

--- src/components/hbvc/trombosisScene/TrombosisScene.tsx
+++ src/components/hbvc/trombosisScene/TrombosisScene.tsx
@@ -8,6 +8,8 @@ import SpotLightToModel from '../SpotLightToModel';
 import CameraFocus from '../CameraFocus';
 import Scene from '../scene/Scene';
 
+import { Stars, Sky } from '@react-three/drei';
+
 export default function TrombosisScene() {
   const { tShowInfo, toggleTInfo } = useStore();
 
@@ -22,6 +24,38 @@ export default function TrombosisScene() {
       }
       onPointerMissed={() => tShowInfo && toggleTInfo()}
     >
+
+       {/* Configuración de cielo nocturno */}
+      <Sky
+        distance={450000} // Distancia grande para efecto de cielo real
+        sunPosition={[0, -1, 0]} // Sol bajo el horizonte (noche)
+        inclination={0}
+        azimuth={0.25}
+        turbidity={0.1} // Cielo muy despejado
+        rayleigh={0.5} // Reducir dispersión atmosférica
+        mieCoefficient={0.005} // Menos partículas en el aire
+        mieDirectionalG={0.8}
+      />
+
+      {/* Campo de estrellas */}
+      <Stars
+        radius={100} // Radio de la esfera de estrellas
+        depth={50} // Profundidad de campo
+        count={5000} // Número de estrellas (aumentar para más densidad)
+        factor={4} // Tamaño de las estrellas
+        saturation={0} // 0 = blanco puro
+        fade // Efecto de desvanecimiento
+        speed={0.2} // Velocidad de rotación lenta
+      />
+
+      {/* Iluminación nocturna*/}
+      <ambientLight intensity={0.05} color="#001144" />
+      <directionalLight
+        intensity={0.3}
+        position={[-10, 10, 5]}
+        color="#003366"
+        castShadow
+      />
       <CameraFocus />
       <TrombosisModel position={[0, 0, 0]} scale={5} onClick={toggleTInfo} />
       <TextTitle3D />
